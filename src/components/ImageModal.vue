24b0c8a2be3a88420dc879b8aefb2b8e227ca02f
AxisMac <erlia@naver.com>
2025-02-04 14:00:09 +0900

    ModalTest2

--- src/components/ImageModal.vue
+++ src/components/ImageModal.vue
@@ -1,19 +1,66 @@
 <template>
-  <div v-if="openImageModal" class="modal"  @click="$emit('closeModal')">
-    <div class="modal-content" @click.stop>
-      <img :src="item1.imgPath" @click="$emit('closeModal')" alt="item1.name" />
+  <div v-if="openImageModal" class="modal"  :style="modalStyle" >
+    <div class="modal-content" :style="modalContentStyle" @click.stop>
+      <img :src="item1.imgPath" @click="$emit('closeModal')"  :alt="item1.name" @load="handleImageLoad" style="cursor: pointer;"/>
     </div>
   </div>
 </template>
 
 
 <script>
+import { ref, computed } from "vue";
+
 export default {
   props: {
     openImageModal: Boolean,
     item1: Object,
   },
+
+setup() {
+    const imgWidth = ref(0);
+    const imgHeight = ref(0);
+
+    // 이미지 크기를 감지하는 함수
+    const handleImageLoad = (event) => {
+      imgWidth.value = event.target.naturalWidth;
+      imgHeight.value = event.target.naturalHeight;
+    };
+
+    // 배경 색상 변경 (가로 vs 세로)
+    const modalStyle = computed(() => ({
+      background: imgWidth.value > imgHeight.value ? "rgba(0, 0, 0, 0.8)" : "rgba(255, 255, 255, 0.9)",
+    }));
+
+    // 모달 크기를 이미지 크기에 맞게 조절
+    const modalContentStyle = computed(() => {
+      const maxWidth = window.innerWidth * 0.9; // 최대 가로 크기 (화면의 90%)
+      const maxHeight = window.innerHeight * 0.9; // 최대 세로 크기 (화면의 90%)
+
+      let width = imgWidth.value;
+      let height = imgHeight.value;
+
+      // 크기가 너무 크면 비율 유지하면서 축소
+      if (width > maxWidth) {
+        const scale = maxWidth / width;
+        width *= scale;
+        height *= scale;
+      }
+      if (height > maxHeight) {
+        const scale = maxHeight / height;
+        width *= scale;
+        height *= scale;
+      }
+
+      return {
+        width: `${width}px`,
+        height: `${height}px`,
+      };
+    });
+
+    return { handleImageLoad, modalStyle, modalContentStyle };
+  },
 };
+
 </script>
 
 <style scoped>
